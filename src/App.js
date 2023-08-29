207d5453b37fe2b07b9c958334d9723e012b92a3
zyzyzly <zeinabzeinali71@gmail.com>
2023-08-29 16:38:06 +0330

    created tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import { Board } from "./components/Board";
+import { ResetButton } from "./components/ResetButton";
+import { ScoreBoard } from "./components/ScoreBoard";
+import "./App.css";
+
+const App = () => {
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const [xPlaying, setXPlaying] = useState(true);
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [scores, setScores] = useState({ xScore: 0, oScore: 0 });
+  const [gameOver, setGameOver] = useState(false);
+
+  const handleBoxClick = (boxIdx) => {
+    // Step 1: Update the board
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying ? "X" : "O";
+      } else {
+        return value;
+      }
+    });
+
+    setBoard(updatedBoard);
+
+    // Step 2: Check if either player has won the game
+    const winner = checkWinner(updatedBoard);
+
+    if (winner) {
+      if (winner === "O") {
+        setScores((prevScores) => ({
+          ...prevScores,
+          oScore: prevScores.oScore + 1,
+        }));
+      } else {
+        setScores((prevScores) => ({
+          ...prevScores,
+          xScore: prevScores.xScore + 1,
+        }));
+      }
+    }
+
+    // Step 3: Change active player
+    setXPlaying(!xPlaying);
+  };
+
+  const checkWinner = (board) => {
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x, y, z] = WIN_CONDITIONS[i];
+
+      // Iterate through win conditions and check if either player satisfies them
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        setGameOver(true);
+        return board[x];
+      }
+    }
+  };
+
+  const resetBoard = () => {
+    setGameOver(false);
+    setBoard(Array(9).fill(null));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScoreBoard scores={scores} xPlaying={xPlaying} />
+      <Board
+        key={board.join("-")}
+        board={board}
+        onClick={gameOver ? resetBoard : handleBoxClick}
+      />
+      <ResetButton resetBoard={resetBoard} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ResetButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ResetButton.css";
+
+export const ResetButton = ({ resetBoard }) => {
+  return (
+    <button className="reset-btn" onClick={resetBoard}>
+      Reset
+    </button>
+  );
+};
